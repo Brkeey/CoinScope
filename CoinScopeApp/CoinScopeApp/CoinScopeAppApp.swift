db9063b97717ce0b1cb0ad283f34b4806e58f5c1
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-12 12:31:55 +0300

    Created Project

---
+++ CoinScopeApp/CoinScopeApp/CoinScopeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinScopeAppApp.swift
+//  CoinScopeApp
+//
+//  Created by Berke Yılmaz on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoinScopeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinScopeApp/CoinScopeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinScopeApp
+//
+//  Created by Berke Yılmaz on 12.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
