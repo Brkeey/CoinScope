aecafb45e54edd8bf14505eff599cf7e93ab894f
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-12 14:20:30 +0300

    Cretaed ColorTheme
    Added color palette and assets

---
+++ CoinScopeApp/CoinScopeApp/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  CoinScopeApp
+//
+//  Created by Berke Yılmaz on 12.02.2025.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
