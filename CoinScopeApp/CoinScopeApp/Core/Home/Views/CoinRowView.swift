2ce05ff305a94b65e9fd4a660e0fb38a5d1a5887
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-13 15:36:45 +0300

    TextFormatter, CoinRowView Update

--- CoinScopeApp/CoinScopeApp/Core/Home/Views/CoinRowView.swift
+++ CoinScopeApp/CoinScopeApp/Core/Home/Views/CoinRowView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct CoinRowView: View {
-    
     let coin: CoinModel
+    let showHoldingColumn: Bool
 
     var body: some View {
         HStack (spacing: 0) {
@@ -24,23 +24,30 @@ struct CoinRowView: View {
                 .padding(.leading, 6)
                 .foregroundStyle(Color.theme.accent)
             Spacer()
+            if showHoldingColumn {
+                VStack (alignment: .trailing) {
+                    Text(coin.currentHoldingsValue.asCurrencyWith6Decimals())
+                        .bold()
+                    Text((coin.currentHoldings ?? 0).asNumberString())
+                        .foregroundStyle(Color.theme.accent)
+                }
+            }
             VStack (alignment: .trailing){
-                Text("\(coin.currentPrice)")
+                Text(coin.currentPrice.asCurrencyWith6Decimals())
                     .bold()
                     .foregroundStyle(Color.theme.accent)
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
+                Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
                     .foregroundColor(
                         (coin.priceChangePercentage24H ?? 0) >= 0 ?
                         Color.theme.green : Color.theme.red
                     )
             }
-            
+            .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
         }
-
-
+        .font(.subheadline)
     }
 }
 
 #Preview {
-    CoinRowView(coin: DeveloperPreview.instance.coin)
+    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingColumn: true)
 }
---
+++ CoinScopeApp/CoinScopeApp/Extensions/Double.swift
@@ -0,0 +1,37 @@
+//
+//  Double.swift
+//  CoinScopeApp
+//
+//  Created by Berke Yılmaz on 13.02.2025.
+//
+
+import Foundation
+
+extension Double {
+    
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        //formatter.locale = .current // default value
+        formatter.currencyCode = "USD" // change currency
+        formatter.currencySymbol = "$" // change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    func asCurrencyWith6Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter6.string(from: number) ?? "0.00"
+    }
+    
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
